7826001fd06baaeaa05903a56f6915853dc19bad
HoHoangLinh <linh0905171522@gmail.com>
2022-04-10 22:34:44 +0700

    ThucHanh

---
+++ ss7_Bien_Datatype_ToanTu/Thuc_Hanh/Sudungtoantu/Sudungtoantu.js
@@ -0,0 +1,11 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("nhập chiều cao")
+inputHeight = prompt("nhập chiều rộng")
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+let x = height * width;
+document.write("Đáp án của bạn là : " +  x);
