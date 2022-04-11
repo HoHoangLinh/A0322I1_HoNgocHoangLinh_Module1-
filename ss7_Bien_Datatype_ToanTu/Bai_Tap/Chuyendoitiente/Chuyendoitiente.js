d62976bbbe4391c5d369e429b769eebbceb09251
HoHoangLinh <linh0905171522@gmail.com>
2022-04-11 15:08:27 +0700

    BaiTap

--- ss7_Bien_Datatype_ToanTu/Bai_Tap/Chuyendoitiente/Chuyendoitiente.js
+++ ss7_Bien_Datatype_ToanTu/Bai_Tap/Chuyendoitiente/Chuyendoitiente.js
@@ -1,6 +1,16 @@
-var VND = 23000;
-var $ = 1;
- if (VND = $) {
+function Convert() {
+    let Amount = document.getElementById("Amount").value;
+    let FromC = document.getElementById("FromC").value;
+    let To = document.getElementById("To").value;
+    let Result;
+    if (FromC == "USD" && To == "VND") {
+        Result = "result : " + (Amount * 23000) + "VND"
+        document.getElementById("Result").innerHTML = Result;
 
- }
-
+    } else if (FromC == "VND" && To == "USD") {
+        Result = "result : " + (Amount / 23000) + "USD";
+    document.getElementById("Result").innerHTML = Result;
+}   else
+        Result = "Không Hợp Lệ"
+        document.getElementById("result").innerHTML = Result;
+    }
