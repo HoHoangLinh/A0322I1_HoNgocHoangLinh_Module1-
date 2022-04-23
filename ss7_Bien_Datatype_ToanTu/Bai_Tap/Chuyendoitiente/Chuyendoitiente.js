afba00bfc04f70330445e6ffa768b2707f267079
HoHoangLinh <linh0905171522@gmail.com>
2022-04-23 12:26:17 +0700

    ThucHanh

--- ss7_Bien_Datatype_ToanTu/Bai_Tap/Chuyendoitiente/Chuyendoitiente.js
+++ ss7_Bien_Datatype_ToanTu/Bai_Tap/Chuyendoitiente/Chuyendoitiente.js
@@ -1,10 +1,10 @@
 function Convert() {
-    var Amount = document.getElementById("Amount").value;
-    var FromC = document.getElementById("FromC").value;
-    var To = document.getElementById("To").value;
-    var Result;
+    let Amount = document.getElementById("Amount").value;
+    let FromC = document.getElementById("FromC").value;
+    let To = document.getElementById("To").value;
+    let Result;
     if (FromC == "USD" && To == "VND") {
-        Result = "result : " + (Amount * 23000) + "VND"
+        Result = "result : " + (Amount * 23000) + "VND";
         document.getElementById("Result").innerHTML = Result;
 
     } else if (FromC == "VND" && To == "USD") {
