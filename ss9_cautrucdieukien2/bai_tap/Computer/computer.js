b818e94db365f2bc2448a08085ac1b2651f6756f
HoHoangLinh <linh0905171522@gmail.com>
2022-04-25 00:19:30 +0700

    BaiTap

--- ss9_cautrucdieukien2/bai_tap/Computer/computer.js
+++ ss9_cautrucdieukien2/bai_tap/Computer/computer.js
@@ -1,25 +1,12 @@
+var value = "";
 function cal(x) {
-    let value = document.getElementById("result").innerText;
-    switch (x) {
-        case "1" :
-        case "2" :
-        case "3" :
-        case "4" :
-        case "5" :
-        case "6" :
-        case "7" :
-        case "8" :
-        case "9" :
-        case "+" :
-            value = value + x;
-        case "-" :
-            value = value - x;
-        case "X" :
-            value = value * x;
-            break;
-        case "=":
-            value = eval(value);
-            break
+    value = value + x
+}
+function tinhtoan(cal){
+    switch (cal) {
+        case '+' :
+            value = value + '+';
+        case '-' :
+            value = value + '-';
     }
-    document.getElementById("result").innerText = value;
 }
