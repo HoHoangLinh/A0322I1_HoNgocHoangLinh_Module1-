30a0c8f76f232a8aeeb71eb6c87116cbf8f5375d
HoHoangLinh <linh0905171522@gmail.com>
2022-05-23 23:20:14 +0700

    BaiTap

--- ss15_function2/BaiTap/UngDungQuanLi/Js.js
+++ ss15_function2/BaiTap/UngDungQuanLi/Js.js
@@ -22,10 +22,60 @@ listproduct()
 function listproduct() {
     let result = "";
     result += "<table id='table'><th>Ten san pham<td></td><td></td><td colspan='2'>" + product.length + " products</td></th>"
-    for (let i = 0 ; i < product.length ; i++) {
-        result += "<tr><td>" + product[i] + "</td>" +
-            "<td> <button onclick="editproduct(" + i + ")">Edit</button> </td>" + "<td> <button onclick="deleteproduct(" + i + ")">Delete</button> </td>" + "</tr>";
+    for (let index = 0; index < product.length; index++) {
+        result += "<tr><td>" + product[index] + "</td>" +
+            "<td><button onclick='edit(" + index + ")'>Edit</button></td>" +
+            "<td><button onclick='deleteProduct(" + index + ")'>Delete</button></td>" +
+            "</tr>";
     }
     result += "</table>";
-    document.getElementById('table').innerText = result;
+    document.getElementById("table").innerHTML = result
+}
+
+function add() {
+    let addproduct = document.getElementById('text');
+    if (addproduct.value === "") {
+        alert("Điền tên sản phẩm")
+    } else {
+        product.push(addproduct.value);
+    }
+    listproduct()
+}
+
+function edit(temp) {
+    let result  = "";
+    result += "<table id='table'><th>Ten san pham<td></td><td colspan='2'>" + product.length + " products</td></th>";
+    for (let index = 0; index < product.length; index++) {
+        if (temp === index) {
+            result += "<tr><td><input id='edit' type='text' value='" + product[index] + "'>"+ "</td>" +
+                "<td><button onclick='update(" + index + ")'>Update</button></td>" +
+                "<td><button onclick='deleteProduct(" + index + ")'>Delete</button></td>" +
+                "</tr>";
+        }else {
+            result += "<tr><td>" + product[index] + "</td>" +
+                "<td><button onclick='edit(" + index + ")'>Edit</button></td>" +
+                "<td><button onclick='deleteProduct(" + index + ")'>Delete</button></td>" +
+                "</tr>";
+        }
+    }
+    result += "</table>";
+    document.getElementById("table").innerHTML = result;
+
+}
+
+function update(temp) {
+    let tenMoi = document.getElementById("edit").value;
+    for (let index = 0; index < danhSachSanPham.length; index++){
+        if(temp === index){
+            product[index] = tenMoi;
+        }
+    }
+    listproduct();
+}
+
+function deleteProduct() {
+    if (confirm("Bạn có chắc muốn xóa sản phẩm " + product[temp] + " ra khỏi danh sách?")){
+        product.splice(temp,1);
+    }
+    listproduct()
 }
