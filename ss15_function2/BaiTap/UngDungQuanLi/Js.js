bf17effbcdaf2dd7eaac8e19d6deb118af48c803
HoHoangLinh <linh0905171522@gmail.com>
2022-05-23 16:00:30 +0700

    BaiTap

--- ss15_function2/BaiTap/UngDungQuanLi/Js.js
+++ ss15_function2/BaiTap/UngDungQuanLi/Js.js
@@ -1,17 +1,31 @@
-let x = 0;
-let arr = Array();
-    function add() {
-        arr[x] = document.getElementById('text').value;
-        alert("Display Product " + arr[x] + " Vị trí " + x);
-        x++
-        document.getElementById('text').value = "";
-    // }
-    // function Display() {
-        let tr = "<tr>";
-        for ( let i = 0 ; i < arr.length ; i++) {
-            tr += "<td>" + arr[i] + "</td>"
-            tr += "<td>" + "<input type='button' value='Edit' onclick='edit(i)'>" + "</td>"
-            tr += "<td>" + "<input type='button' value='Delete'>" + "</td>" + "</tr>"
-        }
-        document.getElementById('test').innerHTML = tr
+// let x = 0;
+// let arr = Array();
+//     function add() {
+//         arr[x] = document.getElementById('text').value;
+//         alert("Display Product " + arr[x] + " Vị trí " + x);
+//         x++
+//         document.getElementById('text').value = "";
+//     // }
+//     // function Display() {
+//         let tr = "<tr>";
+//         for ( let i = 0 ; i < arr.length ; i++) {
+//             tr += "<td>" + arr[i] + "</td>"
+//             tr += "<td>" + "<input type='button' value='Edit' onclick='edit(i)'>" + "</td>"
+//             tr += "<td>" + "<input type='button' value='Delete'>" + "</td>" + "</tr>"
+//         }
+//         document.getElementById('test').innerHTML = tr
+//     }
+
+let product = ["Apple","SamSung","Nokia"];
+listproduct()
+
+function listproduct() {
+    let result = "";
+    result += "<table id='table'><th>Ten san pham<td></td><td></td><td colspan='2'>" + product.length + " products</td></th>"
+    for (let i = 0 ; i < product.length ; i++) {
+        result += "<tr><td>" + product[i] + "</td>" +
+            "<td> <button onclick="editproduct(" + i + ")">Edit</button> </td>" + "<td> <button onclick="deleteproduct(" + i + ")">Delete</button> </td>" + "</tr>";
     }
+    result += "</table>";
+    document.getElementById('table').innerText = result;
+}
